360450659d8e2c5f2bf56dafffb2cc6075d6e4e0
aakashgoel12 <aakash.goel@fractal.ai>
2020-02-22 13:36:22 +0530

    add save data function

--- read_save.py
+++ read_save.py
@@ -1,6 +1,8 @@
 import os,glob,gc
 import pandas as pd
+import pickle
 
+## read either csv, xlsx file
 def read_data(input_file_path, delimiter=",", skiprows=0):
     filename, file_extension = os.path.splitext(input_file_path)
     if file_extension == '.csv':
@@ -21,3 +23,24 @@ def read_data(input_file_path, delimiter=",", skiprows=0):
     except:
         print("Please input file in {} or {} format".format('.csv', '.xlsx'))
         return None
+
+## save either in csv, xlsx, pkl format
+def save_data(data, file_path, encoding='utf-8',index_status=False):
+    filename, file_extension = os.path.splitext(input_file_path)
+    if file_extension == '.csv':
+        try:
+            data.to_csv(file_path, encoding=encoding, index=index_status)
+        except Exception as e:
+            print("Error in saving file: {}".format(e))
+    elif file_extension == '.xlsx' or file_extension == '.xls':
+        try:
+            data.to_excel(file_path, encoding=encoding, index=index_status)
+        except Exception as e:
+            print("Error in saving file: {}".format(e))
+    elif file_extension == '.pkl' or file_extension == '.pickle'
+        try:
+            with open(file_path,'wb') as f:
+                pickle.dump(data,f)
+                f.close()
+        except Exception as e:
+            print("Error in saving file: {}".format(e))
